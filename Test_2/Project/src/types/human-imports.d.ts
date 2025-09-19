844eb30406805786e9e7b116bd9baf222d4b5987
Himel-Tasrif <himel35-1078@diu.edu.bd>
2025-09-20 04:07:53 +0600

    Updated

---
+++ Test_2/Project/src/types/human-imports.d.ts
@@ -0,0 +1,12 @@
+declare module "@vladmandic/human" {
+  const HumanCtor: any;
+  export default HumanCtor;
+}
+declare module "@vladmandic/human/dist/human.esm.js" {
+  const HumanCtor: any;
+  export default HumanCtor;
+}
+declare module "@vladmandic/human/dist/human.esm-nobundle.js" {
+  const HumanCtor: any;
+  export default HumanCtor;
+}
